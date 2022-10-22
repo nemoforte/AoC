3962e7f1ed3faf0c2a695bbab1548a79136689aa
Marcin <84280969+nemoforte@users.noreply.github.com>
2022-10-22 20:10:40 +0200

    Complete AoC day3

--- day_3_binary_diagnostic_part_2.dart
+++ day_3_binary_diagnostic_part_2.dart
@@ -11,60 +11,57 @@ Future<void> main() async {
   List<String> temp = <String>[];
   String lookup;
 
-  while (o2.length > 1) {
-    for (int j = 0; j < inputlist[0].length - 1; j++) {
-      int c = 0;
+  for (int j = 0; j < inputlist[0].length - 1; j++) {
+    int c = 0;
+    if (o2.length > 1) {
       for (int i = 0; i < o2.length; i++) {
         if (o2[i][j] == '0') {
           c++;
         }
       }
-      print('c = $c');
       if (c > o2.length / 2) {
         lookup = '0';
       } else {
         lookup = '1';
       }
-      print('lookup $lookup');
-
       for (int i = 0; i < o2.length; i++) {
         if (o2[i][j] == lookup) {
           temp.add(o2[i]);
         }
       }
+      o2 = temp;
+      temp = <String>[];
     }
-    o2.clear();
   }
+
+  for (int j = 0; j < inputlist[0].length - 1; j++) {
+    int c = 0;
+    if (co2.length > 1) {
+      for (int i = 0; i < co2.length; i++) {
+        if (co2[i][j] == '0') {
+          c++;
+        }
+      }
+      if (c <= co2.length / 2) {
+        lookup = '0';
+      } else {
+        lookup = '1';
+      }
+      for (int i = 0; i < co2.length; i++) {
+        if (co2[i][j] == lookup) {
+          temp.add(co2[i]);
+        }
+      }
+      co2 = temp;
+      temp = <String>[];
+    }
+  }
+
+  // print(o2[0]);
+  // print(co2[0]);
+  int lsr =
+      int.parse(o2[0].toString(), radix: 2) * int.parse(co2[0].toString(), radix: 2);
+  // print(int.parse(o2[0].toString(), radix: 2));
+  // print(int.parse(co2[0].toString(), radix: 2));
+  print(lsr);
 }
-  // for (int j = 0; j < inputlist[0].length - 1; j++) {
-  //   int c = 0;
-  //   for (int i = 0; i < co2.length; i++) {
-  //     if (co2[i][j] == '0') {
-  //       c++;
-  //     }
-  //   }
-  //   List<String> temp = <String>[];
-  //   if (c <= co2.length) {
-  //     for (int i = 0; i < co2.length; i++) {
-  //       if (co2[i][j] == '0') {
-  //         temp.add(o2[i][j]);
-  //       }
-  //     }
-  //   } else {
-  //     for (int i = 0; i < co2.length; i++) {
-  //       if (co2[i][j] == '1') {
-  //         temp.add(co2[i][j]);
-  //       }
-  //     }
-  //   }
-  //   co2 = temp;
-  // }
-
-  // print(o2);
-  // print(co2);
-  // int lsr =
-  //     int.parse(o2.toString(), radix: 2) * int.parse(co2.toString(), radix: 2);
-  // print(int.parse(o2.toString(), radix: 2));
-  // print(int.parse(co2.toString(), radix: 2));
-  // print(lsr);
-
