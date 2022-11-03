b6d1c744c89598f1f1fd036015f36ac4f0528b7b
Marcin <jaykubiak0201@gmail.com>
2022-11-03 20:31:21 +0100

    AoC day_10_part_1 completed

--- day_10_syntax_scoring_part_1.dart
+++ day_10_syntax_scoring_part_1.dart
@@ -6,5 +6,92 @@ import 'dart:io';
 Future<void> main() async {
   final File file = File('inputs/day_10.txt');
   final String contents = await file.readAsString();
-  List<String> inputlist = contents.split('\n');  
+  List<String> inputlist = contents.split('\n');
+  for (int j = 0; j < inputlist.length; j++) {
+    inputlist[j] = inputlist[j].trim();
+  }
+
+  // input list to array 2d cause idk how to operate on Strings
+  List<List<String>> array =
+      List<List<String>>.generate(inputlist.length, (int j) => <String>[]);
+  for (int j = 0; j < inputlist.length; j++) {
+    for (int i = 0; i < inputlist[j].length; i++) {
+      array[j].add(inputlist[j][i]);
+    }
+  }
+
+  // reducing chunks
+  for (int j = 0; j < array.length; j++) {
+    // print(array[j].join(' '));
+    for (int i = 0; i < array[j].length - 1; i++) {
+      if (array[j][i] == '(' && array[j][i + 1] == ')') {
+        array[j].removeAt(i);
+        array[j].removeAt(i);
+        // print(array[j].join(' '));
+        if (i > 0) {
+          i -= 2;
+        } else {
+          i--;
+        }
+      } else if (array[j][i] == '[' && array[j][i + 1] == ']') {
+        array[j].removeAt(i);
+        array[j].removeAt(i);
+        // print(array[j].join(' '));
+        if (i > 0) {
+          i -= 2;
+        } else {
+          i--;
+        }
+      } else if (array[j][i] == '{' && array[j][i + 1] == '}') {
+        array[j].removeAt(i);
+        array[j].removeAt(i);
+        // print(array[j].join(' '));
+        if (i > 0) {
+          i -= 2;
+        } else {
+          i--;
+        }
+      } else if (array[j][i] == '<' && array[j][i + 1] == '>') {
+        array[j].removeAt(i);
+        array[j].removeAt(i);
+        // print(array[j].join(' '));
+        if (i > 0) {
+          i -= 2;
+        } else {
+          i--;
+        }
+      }
+    }
+  }
+
+  // // show reduced array
+  // for (int j = 0; j < array.length; j++) {
+  //   print(array[j].join(''));
+  // }
+
+  // calculating results
+  int sum = 0;
+  for (int j = 0; j < array.length; j++) {
+    outerLoop:
+    for (int i = 0; i < array[j].length; i++) {
+      if (array[j][i] == ')') {
+        sum += 3;
+        // print(j);
+        break outerLoop;
+      } else if (array[j][i] == ']') {
+        sum += 57;
+        // print(j);
+        break outerLoop;
+      } else if (array[j][i] == '}') {
+        sum += 1197;
+        // print(j);
+        break outerLoop;
+      } else if (array[j][i] == '>') {
+        sum += 25137;
+        // print(j);
+        break outerLoop;
+      }
+    }
+  }
+  print(sum);
 }
